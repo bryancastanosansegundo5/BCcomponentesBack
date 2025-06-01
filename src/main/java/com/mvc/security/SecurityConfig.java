3303f5c50ac749037cb25f4656c82f510d50b42c
bryancastanosansegundo5 <bryanlopesrider100@gmail.com>
2025-06-01 23:27:56 +0200

    Configurar SecurityConfig y CorsConfig

--- src/main/java/com/mvc/security/SecurityConfig.java
+++ src/main/java/com/mvc/security/SecurityConfig.java
@@ -6,16 +6,24 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
-
+import org.springframework.security.config.Customizer;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-    @Bean
+    // @Bean
+    // public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    //     http
+    //             .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+    //             .csrf(csrf -> csrf.disable());
+    //     return http.build();
+    // }
+     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
-                .csrf(csrf -> csrf.disable());
+            .cors(Customizer.withDefaults())
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
         return http.build();
     }
 
